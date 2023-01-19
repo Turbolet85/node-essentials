f8dde6b6cf8509d288b3e2a14f3acaf639200245
Anton Soldatov <turbolet85@gmail.com>
2023-01-19 20:51:53 +0100

    async_promise_nodenative_style

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-const _ = require('lodash');
+const { readFile, writeFile } = require('fs').promises;
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-const items = [1, [2, [3, [4]]]];
-const newItem = _.flattenDeep(items);
-console.log(newItem);
+// const getText = (path) => {
+// 	return new Promise((resolve, reject) => {
+// 		readFile(path, 'utf8', (err, data) => {
+// 			if (err) {
+// 				reject(err);
+// 			} else {
+// 				resolve(data);
+// 			}
+// 		});
+// 	});
+// };
+
+// getText('./context/first.txt')
+// 	.then((result) => console.log(result))
+// 	.catch((err) => console.log(err));
+
+const start = async () => {
+	try {
+		const first = await readFile('./context/first.txt', 'utf8');
+		const second = await readFile('./context/second.txt', 'utf8');
+		await writeFile(
+			'./context/result-mind-grenade.txt',
+			`THIS IS AWESOME: ${first}, ${second}`
+		);
+		console.log(first, second);
+	} catch (error) {
+		console.log('error');
+	}
+};
+
+start();
